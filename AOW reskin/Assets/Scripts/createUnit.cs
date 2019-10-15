d8f0bcf69f17d82b93fac0faa07c61ee75bb3994
leavinth <32339002+leavinth@users.noreply.github.com>
2019-10-14 18:05:20 -0700

    Menu Mechanic
    just added the menu mechanic and allowed for units to be created with a delay that has a visible indicator.

---
+++ AOW reskin/Assets/Scripts/createUnit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createUnit : MonoBehaviour
+{
+    //static
+    static float timer;                                                                                                         //static to make the delay constant accrosed all unit instances
+    //config
+    [SerializeField] GameObject unit;
+    [SerializeField] GameObject unitHolder;
+    [SerializeField] Vector3 Vector;
+    [SerializeField] GameObject loader;
+    [SerializeField] float waitTime = 300;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Check the tag and check the age: is it the correct unit type?
+    }
+
+    private void Update()
+    {
+        timer++;
+        loader.transform.localScale = new Vector3(Mathf.Clamp((timer / waitTime)*2,0,2), loader.transform.localScale.y);        // visualize delay
+    }
+
+    public void instantiateUnit()
+    {
+        if(timer >= waitTime)
+        {
+            timer = 0;
+            Instantiate(unit, Vector, new Quaternion(0f, 0f, 0f, 0f), unitHolder.transform);                                    //if the delay is over create clone of unit prefab at starting position
+        }
+        
+    }
+}
